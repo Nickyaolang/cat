5214e48d95cbe80651c58e5179cdd59dd999e15a
aolang <2222015222@qq.com>
2022-04-01 18:30:28 +0800

    init

---
+++ cat-service/service-system/src/main/java/service/system/SystemApplication.java
@@ -0,0 +1,15 @@
+package service.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author al
+ * @Date 2022/4/1
+ */
+@SpringBootApplication
+public class SystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class,args);
+    }
+}
